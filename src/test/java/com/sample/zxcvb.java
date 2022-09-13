1eb2bc9dfda13bd3064bf662dfd75d9743a6dfd4
Umamani <Umamani@DESKTOP-EM3OVKO>
2022-09-13 22:13:20 +0530

    zxcvbn

---
+++ src/test/java/com/sample/zxcvb.java
@@ -0,0 +1,16 @@
+package com.sample;
+
+public class zxcvb {
+
+	public static void main(String[] args) {
+		System.out.println("This is my sample project");
+		System.out.println("Sample1");
+
+	
+		System.out.println("This is my sample2 project");
+		System.out.println("Sample2");
+
+
+	}
+
+}
